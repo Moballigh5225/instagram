9e2247fb34642e150423ed5e3a2358c9e9d7ea37
Moballighul islam <moballigh5225@gmail.com>
2023-08-08 12:18:19 +0530

    user stories template and data created and added to the feed

---
+++ components/Feed.js
@@ -0,0 +1,17 @@
+import Stories from "./Stories";
+
+export default function Feed() {
+  return (
+    <main>
+      <section>
+        {/* stories */}
+        <Stories />
+        {/* posts */}
+      </section>
+      <section>
+        {/* mini profiles */}
+        {/* Suggestions */}
+      </section>
+    </main>
+  );
+}
---
+++ components/Story.js
@@ -0,0 +1,8 @@
+export default function Story({ image, username }) {
+  return (
+    <div>
+      <img src={image} alt={username} />
+      <p>{username}</p>
+    </div>
+  );
+}
