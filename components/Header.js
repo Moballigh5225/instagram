d1cc52057d94207a5746fb186fc382aceecc8bf1
Moballighul islam <moballigh5225@gmail.com>
2023-08-08 10:59:11 +0530

    sticky header implemented

--- components/Header.js
+++ components/Header.js
@@ -8,42 +8,44 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      <div className="cursor-pointer h-24 w-10 relative  lg:hidden ">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png?20210403190622"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
-      {/* Right  */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className=" hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="http://m.gettywallpapers.com/wp-content/uploads/2023/06/Monkey-D.-Luffy-Avatar.jpg"
-          alt="user-image"
-          className="h-10 rounded-full"
-        />
+        <div className="cursor-pointer h-24 w-10 relative  lg:hidden ">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png?20210403190622"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* Right  */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className=" hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="http://m.gettywallpapers.com/wp-content/uploads/2023/06/Monkey-D.-Luffy-Avatar.jpg"
+            alt="user-image"
+            className="h-10 rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
