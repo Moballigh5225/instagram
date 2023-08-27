920dd0d8ea461d2d31dfd3e569af7a00e77bc360
Moballighul islam <moballigh5225@gmail.com>
2023-08-28 00:47:25 +0530

    installed atom for global state management and created Modal

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Feed from "../components/Feed";
+import UploadModal from "../components/UploadModal";
 
 export default function Home() {
   return (
@@ -14,8 +15,8 @@ export default function Home() {
       <Header />
       {/* feed  */}
       <Feed />
-
       {/* Modal */}
+      <UploadModal />
     </div>
   );
 }
