3162163e3c8eeb71c935ca145be2e89494a27228
Moballighul islam <moballigh5225@gmail.com>
2023-08-31 16:45:44 +0530

    connected the database with the ui and retrieved the data using onSnapshot method

--- components/Posts.js
+++ components/Posts.js
@@ -1,34 +1,30 @@
 import Post from "./Post";
+import { useState, useEffect } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Posts() {
-  const posts = [
-    {
-      id: "1",
-      username: "codewithsadiq",
-      userImg:
-        "http://m.gettywallpapers.com/wp-content/uploads/2023/06/Monkey-D.-Luffy-Avatar.jpg",
-      img: "https://plus.unsplash.com/premium_photo-1690481525616-454bacd58537?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-      caption: "Sunset Picture",
-    },
-    {
-      id: "2",
-      username: "codewithanny",
-      userImg:
-        "http://m.gettywallpapers.com/wp-content/uploads/2023/06/Monkey-D.-Luffy-Avatar.jpg",
-      img: "https://images.unsplash.com/photo-1690921822670-5929218ab41d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
-      caption: "Well its a sunny day !",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, []);
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
